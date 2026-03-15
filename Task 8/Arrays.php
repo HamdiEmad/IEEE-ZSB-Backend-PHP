c69aa352b3bb59f018125195768b3852520939d8
Hamdi Emad <hamdyemadelgohary2004@gmail.com>
2026-03-15 23:43:20 +0200

    Arrays (1D & 2D)

---
+++ Task 8/Arrays.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Testing</title>
+</head>
+<body>
+    <h1> Some programming languages </h1>
+    <?php
+        $langs = ["PHP", "C", "Java", "Python"];
+    ?>
+    <ul>
+        <?php foreach ($langs as $lang) {
+            echo "<li>$lang</li>";
+        } ?>
+    </ul>
+    <br>
+    <p> <?= $langs[0] ?> </p>
+    <br>
+    <h1> Associative arrays (2D Arrays) </h1>
+    <?php
+        $player = [
+            [
+                'name' => 'Osama',
+                'Age' => '52',
+                'Address' => ''
+            ], [], []];
+    ?>
+</body>
+</html>
+
+
