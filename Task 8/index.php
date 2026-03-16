b00da5ed09b6c4fa0e079eca71caff9572f7a62a
Hamdi Emad <hamdyemadelgohary2004@gmail.com>
2026-03-16 14:11:02 +0200

    Connect to MySQL using PDO

--- Task 8/index.php
+++ Task 8/index.php
@@ -1,1 +1,16 @@
 <?php
+
+require 'functions.php';
+//require 'router.php';
+
+$dsn = "mysql:host=localhost;port=3306;dbname=ramadan_mystrey;charset=utf8mb4";
+
+$PDO = new PDO($dsn, 'root', 'root');
+
+$statement = $PDO->prepare("SELECT * FROM ramadan_mystrey.sobia_king_sales;");
+
+$statement->execute();
+
+$result = $statement->fetchAll();
+
+dd($result);
