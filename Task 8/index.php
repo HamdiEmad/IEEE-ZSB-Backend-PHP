0a45cffd39dae54829f9672b45619c9b2c51f441
Hamdi Emad <hamdyemadelgohary2004@gmail.com>
2026-03-16 14:42:04 +0200

    Updating index.php file

--- Task 8/index.php
+++ Task 8/index.php
@@ -4,7 +4,8 @@ require 'functions.php';
 //require 'router.php';
 require 'Database.php';
 
+$config = require "config.php";
 
-$db = new Database();
+$db = new Database($config);
 $result = $db->query("SELECT * FROM ramadan_mystrey.sobia_king_sales");
 dd($result);
