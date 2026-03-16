ce8a2382b127cdbd6bb78fc24541288792601872
Hamdi Emad <hamdyemadelgohary2004@gmail.com>
2026-03-16 13:31:32 +0200

    Adding views

--- Task 8/views/partials/nav.php
+++ Task 8/views/partials/nav.php
@@ -9,8 +9,8 @@
                     <div class="ml-10 flex items-baseline space-x-4">
                         <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-white/5 hover:text-white" -->
                         <a href="/" class="rounded-md px-3 py-2 text-sm font-medium <?= urlIS('/') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-white/5 hover:text-white' ?>">Home</a>
-                        <a href="/about.php" class="rounded-md px-3 py-2 text-sm font-medium <?= $_SERVER['REQUEST_URI'] === '/about.php' ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-white/5 hover:text-white' ?>">About</a>
-                        <a href="/contact.php" class="rounded-md px-3 py-2 text-sm font-medium <?= $_SERVER['REQUEST_URI'] === '/contact.php' ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-white/5 hover:text-white' ?>">Contact</a>
+                        <a href="/about" class="rounded-md px-3 py-2 text-sm font-medium <?= $_SERVER['REQUEST_URI'] === '/about' ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-white/5 hover:text-white' ?>">About</a>
+                        <a href="/contact" class="rounded-md px-3 py-2 text-sm font-medium <?= $_SERVER['REQUEST_URI'] === '/contact' ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-white/5 hover:text-white' ?>">Contact</a>
                     </div>
                 </div>
             </div>
