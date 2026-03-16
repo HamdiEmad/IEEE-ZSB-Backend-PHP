7d13cd3a4868f678de24179c335f7786dbbec972
Hamdi Emad <hamdyemadelgohary2004@gmail.com>
2026-03-16 13:28:36 +0200

    Adding controllers

---
+++ Task 8/views/partials/head.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en" class="h-full bg-gray-100">
+<head>
+    <meta charset="UTF-8">
+    <title>Testing</title>
+    <script src="https://cdn.tailwindcss.com"></script> -->
+</head>
+<body class="h-full">
+<!-- Include this script tag or install `@tailwindplus/elements` via npm: -->
+<!-- <script src="https://cdn.jsdelivr.net/npm/@tailwindplus/elements@1" type="module"></script> -->
+<!--
+  This example requires updating your template:
+
+  ```
+  <html class="h-full bg-gray-100">
+  <body class="h-full">
+  ```
+-->
+<div class="min-h-full">
