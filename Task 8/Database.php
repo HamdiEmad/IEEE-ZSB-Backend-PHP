4cb9f7163eeecd82b0e89fbd5ef62342c5358c2d
Hamdi Emad <hamdyemadelgohary2004@gmail.com>
2026-03-16 14:42:45 +0200

    Environments and Configuration Flexibility

--- Task 8/Database.php
+++ Task 8/Database.php
@@ -2,8 +2,9 @@
 
 class Database{
     public $connection;
-    public function __construct(){
-        $dsn = "mysql:host=localhost;port=3306;dbname=ramadan_mystrey;charset=utf8mb4";
+    public function __construct($config){
+
+        $dsn = 'mysql:' . http_build_query($config, '', ';');
         $this->connection = new PDO($dsn, 'root', 'root');
     }
     public function query($query) {
