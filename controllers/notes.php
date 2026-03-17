2cc2236227765a13fd1314e7be1f0baa8a9d1b87
Gohary <154706380+HamdiEmad@users.noreply.github.com>
2026-03-17 18:49:38 +0200

    Change heading and query in notes.php

--- controllers/notes.php
+++ controllers/notes.php
@@ -3,9 +3,8 @@
 $config = require 'config.php';
 $db = new Database($config);
 
-$heading = "Notes";
+$heading = "My Notes";
 
-$notes = $db->query('SELECT * FROM ramadan_mystrey.sobia_king_sales WHERE order_details like "2 Sobia, 1 Kharoub%"');
+$notes = $db->query('SELECT * FROM ramadan_mystrey.sobia_king_sales WHERE customer_phone like "011-62%";')->get();
 
 require "views/notes.view.php";
-
