033fa1cbe099e286a7465ed07db026e014ecc5be
Gohary <154706380+HamdiEmad@users.noreply.github.com>
2026-03-17 21:23:03 +0200

    Refactor note creation validation using Validator class

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -1,17 +1,16 @@
 <?php
 
-$config = require 'config.php';
+require 'Task 9/Validator.php';
+
+$config = require 'Task 9/config.php';
 $db = new Database($config);
 
 $heading = "Create note";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $errors =[];
-    if (strlen($_POST['body']) === 0) {
-        $errors['body'] = "A body is required";
-    }
-    if (strlen($_POST['body']) > 1000) {
-        $errors['body'] = "The body cannot exceed 1000 characters";
+    if (! Validator::string($_POST['body'], 1, 1000)) {
+        $errors['body'] = "A body of no more than 1000 characters is required";
     }
     if (empty($errors)) {
         $db->query('INSERT INTO notes(body, user_id) VALUES(:body, :user_id)', [
