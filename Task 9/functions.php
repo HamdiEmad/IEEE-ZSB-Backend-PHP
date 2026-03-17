6b54eb5b60292bfbe3b3c7314782c80f2be9edc1
Gohary <154706380+HamdiEmad@users.noreply.github.com>
2026-03-17 21:27:48 +0200

    Task 9

---
+++ Task 9/functions.php
@@ -0,0 +1,18 @@
+<?php
+
+function dd($value) {
+    echo "<pre>";
+    var_dump($value);
+    echo "</pre>";
+    die();
+}
+
+function urlIS($url) {
+    return $_SERVER['REQUEST_URI'] === $url;
+}
+
+function authorize($condition, $status = Response::FORBIDDEN) {
+    if (! $condition) {
+        abort($status);
+    }
+}
