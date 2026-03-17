9e6765375f0d5374f4cfc6d1a5a5d1f80f9dbe72
Gohary <154706380+HamdiEmad@users.noreply.github.com>
2026-03-17 18:48:34 +0200

    Implement note creation functionality
    Added database insertion for note creation.

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -1,9 +1,15 @@
 <?php
 
+$config = require 'config.php';
+$db = new Database($config);
+
 $heading = "Create note";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    dd($_POST);
+    $db->query('INSERT INTO notes(body, user_id) VALUES(:body, :user_id)', [
+        'body' => $_POST['body'],
+        'user_id' => $currentUser->id,
+    ]);
 }
 
 require 'views/note-create.view.php';
