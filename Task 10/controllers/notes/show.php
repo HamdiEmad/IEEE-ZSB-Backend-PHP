e1f096240dd1200c681641a306f6a99d46f24681
Gohary <154706380+HamdiEmad@users.noreply.github.com>
2026-03-23 21:51:01 +0200

    Update show.php

--- Task 10/controllers/notes/show.php
+++ Task 10/controllers/notes/show.php
@@ -6,27 +6,12 @@ $config = require base_path('config.php');
 $db = new Database($config);
 $currentTransactionId = 134;
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $note = $db->query('SELECT * FROM ramadan_mystrey.sobia_king_sales WHERE transaction_id = :id', [
-        'id' => $_GET['transaction_id']])->findOrFail();
+$note = $db->query('SELECT * FROM ramadan_mystrey.sobia_king_sales WHERE transaction_id = :id', [
+    'id' => $_GET['transaction_id']])->findOrFail();
 
-    authorize($note['transaction_id'] === $currentTransactionId);
+authorize($note['transaction_id'] === $currentTransactionId);
 
-    $db->query("DELETE FROM notes WHERE id = :id", [
-        'id' => $_GET['id '],
-    ]);
-    header("Location:/notes");
-    exit();
-} else {
-
-    $note = $db->query('SELECT * FROM ramadan_mystrey.sobia_king_sales WHERE transaction_id = :id', [
-        'id' => $_GET['transaction_id']])->findOrFail();
-
-    authorize($note['transaction_id'] === $currentTransactionId);
-
-    view("notes/show.view.php", [
-        'heading' => 'Note',
-        'note' => $note
-    ]);
-}
-
+view("notes/show.view.php", [
+    'heading' => 'Note',
+    'note' => $note
+]);
