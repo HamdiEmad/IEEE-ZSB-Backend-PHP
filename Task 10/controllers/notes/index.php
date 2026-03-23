278b1f5e4fd8ea969f23ccd64a6f0823200a219f
Gohary <154706380+HamdiEmad@users.noreply.github.com>
2026-03-23 19:16:27 +0200

    Update notes controller to use view function
    Refactor notes controller to use view function for rendering.

--- Task 10/controllers/notes/index.php
+++ Task 10/controllers/notes/index.php
@@ -1,10 +1,11 @@
 <?php
 
-$config = require 'config.php';
+$config = require base_path('config.php');
 $db = new Database($config);
-
-$heading = "My Notes";
 
 $notes = $db->query('SELECT * FROM ramadan_mystrey.sobia_king_sales WHERE customer_phone like "011-62%";')->get();
 
-require "views/index.view.php";
+view("notes/index.view.php", [
+   'heading' => 'My Notes',
+    'notes' => $notes,
+]);
