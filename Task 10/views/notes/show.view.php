1ec28a88e321742b97e47bbe2eabb467635dfed2
Gohary <154706380+HamdiEmad@users.noreply.github.com>
2026-03-23 21:33:33 +0200

    Add DELETE method input for note deletion

--- Task 10/views/notes/show.view.php
+++ Task 10/views/notes/show.view.php
@@ -14,6 +14,7 @@
                 <a href="/notes/create" class="text-blue-500 hover:underline">Create a note</a>
             </p>
             <form class="mt-6" method="post">
+                <input type="hidden" name="_method" value="DELETE">
                 <input type="hidden" name="id" value="<?= $note['id'] ?>">
                 <button class="text-sm text-red-500">Delete</button>
             </form>
