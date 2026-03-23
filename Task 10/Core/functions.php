5ed35e230db1b252d4e5c031db56d5c3924ae7ba
Gohary <154706380+HamdiEmad@users.noreply.github.com>
2026-03-23 19:17:38 +0200

    Adding a Core directory to save classes

---
+++ Task 10/Core/functions.php
@@ -0,0 +1,29 @@
+<?php
+
+function dd($value) {
+    echo "<pre>";
+    var_dump($value);
+    echo "</pre>";
+    die();
+}
+
+function urlIS($url) {
+    return $_SERVER['REQUEST_URI'] === $url;
+}
+
+function authorize($condition, $status = Response::FORBIDDEN) {
+    if (! $condition) {
+        abort($status);
+    }
+}
+
+function base_path($path)
+{
+    return BASE_PATH . $path;
+}
+
+function view($path, $attributes = [])
+{
+    extract($attributes);
+    require base_path("views/" . $path);
+}
