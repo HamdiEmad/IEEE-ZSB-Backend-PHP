9e3505765caef7e283b776976c30475fe6450c38
Gohary <154706380+HamdiEmad@users.noreply.github.com>
2026-03-23 20:29:14 +0200

    Implement note deletion in show.php
    Add functionality to delete a note when the request method is POST.

--- Task 10/controllers/notes/show.php
+++ Task 10/controllers/notes/show.php
@@ -4,15 +4,29 @@ use Core\Database;
 
 $config = require base_path('config.php');
 $db = new Database($config);
-
 $currentTransactionId = 134;
 
-$note = $db->query('SELECT * FROM ramadan_mystrey.sobia_king_sales WHERE transaction_id = :id', [
-    'id' => $_GET['transaction_id']])->findOrFail();
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $note = $db->query('SELECT * FROM ramadan_mystrey.sobia_king_sales WHERE transaction_id = :id', [
+        'id' => $_GET['transaction_id']])->findOrFail();
 
-authorize($note['transaction_id'] === $currentTransactionId);
+    authorize($note['transaction_id'] === $currentTransactionId);
 
-view("notes/show.view.php", [
-    'heading' => 'Note',
-    'note' => $note
-]);
+    $db->query("DELETE FROM notes WHERE id = :id", [
+        'id' => $_GET['id '],
+    ]);
+    header("Location:/notes");
+    exit();
+} else {
+
+    $note = $db->query('SELECT * FROM ramadan_mystrey.sobia_king_sales WHERE transaction_id = :id', [
+        'id' => $_GET['transaction_id']])->findOrFail();
+
+    authorize($note['transaction_id'] === $currentTransactionId);
+
+    view("notes/show.view.php", [
+        'heading' => 'Note',
+        'note' => $note
+    ]);
+}
+
