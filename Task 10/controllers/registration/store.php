c67c3d15e236592357f448f269ca763609a958ba
Gohary <154706380+HamdiEmad@users.noreply.github.com>
2026-03-24 17:24:21 +0200

    Update store.php to add hashed pass to DB

--- Task 10/controllers/registration/store.php
+++ Task 10/controllers/registration/store.php
@@ -35,7 +35,7 @@ if ($user) {
 } else {
     $db->query("INSERT INTO users (email, password) VALUES (:email, :password)", [
         'email' => $email,
-        'password' => $password
+        'password' => password_hash($password, PASSWORD_DEFAULT)
     ]);
     $_SESSION['logged_in'] = true;
     $_SESSION['user'] = [
