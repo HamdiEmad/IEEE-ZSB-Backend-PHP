cc1d6e9f0244681cae85ec1062d8b00559098f7c
Gohary <154706380+HamdiEmad@users.noreply.github.com>
2026-03-24 18:17:30 +0200

    Update nav.php to add logout view

--- Task 10/views/partials/nav.php
+++ Task 10/views/partials/nav.php
@@ -10,7 +10,9 @@
                         <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-white/5 hover:text-white" -->
                         <a href="/" class="rounded-md px-3 py-2 text-sm font-medium <?= urlIS('/') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-white/5 hover:text-white' ?>">Home</a>
                         <a href="/about" class="rounded-md px-3 py-2 text-sm font-medium <?= $_SERVER['REQUEST_URI'] === '/about' ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-white/5 hover:text-white' ?>">About</a>
-                        <a href="/notes" class="rounded-md px-3 py-2 text-sm font-medium <?= $_SERVER['REQUEST_URI'] === '/notes' ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-white/5 hover:text-white' ?>">Notes</a>
+                        <?php if ($_SESSION['user'] ?? false) : ?>
+                            <a href="/notes" class="rounded-md px-3 py-2 text-sm font-medium <?= $_SERVER['REQUEST_URI'] === '/notes' ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-white/5 hover:text-white' ?>">Notes</a>
+                        <? endif; ?>
                         <a href="/contact" class="rounded-md px-3 py-2 text-sm font-medium <?= $_SERVER['REQUEST_URI'] === '/contact' ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-white/5 hover:text-white' ?>">Contact</a>
                     </div>
                 </div>
@@ -27,15 +29,16 @@
 
                     <!-- Profile dropdown -->
                     <el-dropdown class="relative ml-3">
-                        <button class="relative flex max-w-xs items-center rounded-full focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">
-                            <span class="absolute -inset-1.5"></span>
-                            <span class="sr-only">Open user menu</span>
-                            <?php if (isset($_SESSION['user']) ? $_SESSION['user'] : false) : ?>
-                                <img src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" class="size-8 rounded-full outline -outline-offset-1 outline-white/10" />
+                            <?php if ($_SESSION['user'] ?? false) : ?>
+                                <button class="relative flex max-w-xs items-center rounded-full focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">
+                                    <span class="absolute -inset-1.5"></span>
+                                    <span class="sr-only">Open user menu</span>
+                                    <img src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" class="size-8 rounded-full outline -outline-offset-1 outline-white/10" />
+                                </button>
                             <?php else : ?>
-                                <a href="/register" class="text-white">Register</a>
+                                <a href="/register" class=" <?= urlIS('/register') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-white/5 hover:text-white' ?>">Register</a>
+                                <a href="/login" class=" <?= urlIS('/login') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-white/5 hover:text-white' ?>">Log in</a>
                             <?php endif; ?>
-                        </button>
 
                         <el-menu anchor="bottom end" popover class="w-48 origin-top-right rounded-md bg-white py-1 shadow-lg outline-1 outline-black/5 transition transition-discrete [--anchor-gap:--spacing(2)] data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in">
                             <a href="#" class="block px-4 py-2 text-sm text-gray-700 focus:bg-gray-100 focus:outline-hidden">Your profile</a>
@@ -44,6 +47,14 @@
                         </el-menu>
                     </el-dropdown>
                 </div>
+                <?php if ($_SESSION['user'] ?? false) : ?>
+                    <div class="ml-3">
+                        <form method="post" action="/session">
+                            <input type="hidden" name="_method" value="DELETE" />
+                            <button class="text-white">Log Out</button>
+                        </form>
+                    </div>
+                <?php endif ?>
             </div>
             <div class="-mr-2 flex md:hidden">
                 <!-- Mobile menu button -->
