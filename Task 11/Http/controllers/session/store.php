70cfaf72c7a906f8b7623537cd8dd7393c51d134
Gohary <154706380+HamdiEmad@users.noreply.github.com>
2026-03-29 00:51:36 +0200

    improve store.php file with an authenticator instance

--- Task 11/Http/controllers/session/store.php
+++ Task 11/Http/controllers/session/store.php
@@ -1,36 +1,21 @@
 <?php
 
-use Core\App;
-use Core\Database;
+use Core\Authenticator;
 use Http\Forms\LoginForm;
-
-$db = App::resolve(Database::class);
 
 $email = $_POST['email'];
 $password = $_POST['password'];
 
 $form = new LoginForm();
-if (! $form->validate($email, $password))
+
+if ($form->validate($email, $password))
 {
-    return view('session/create.view.php', [
-        'errors' => $errors
-    ]);
-}
-
-$user = $db->query('SELECT * FROM users WHERE email = :email', [
-    'email' => $email
-])->find();
-
-if ($user) {
-    if (password_verify($password, $user['password'])) {
-        login([
-            'email' => $email,
-        ]);
-        header('Location: /');
-        exit();
+    if ((new Authenticator)->attempt($email, $password)) {
+        redirect('/');
     }
+    $form->error('email', 'No user with that email address and password');
 }
 
 return view('session/create.view.php', [
-    'errors' => 'No user with that email address and password'
+    'errors' => $form->errors()
 ]);
