2abaa523e4cd72fa7ca9b5557d1f06d616517b82
Gohary <154706380+HamdiEmad@users.noreply.github.com>
2026-04-10 23:01:28 +0200

    Add vendor directory

---
+++ Task 11/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'hamdi/task11',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '0942139d22b4a4a8df6f87c19889df0403f44407',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'hamdi/task11' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '0942139d22b4a4a8df6f87c19889df0403f44407',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
