39fa738fbe50f95824244ae4503790e3fa2e6fda
Gohary <154706380+HamdiEmad@users.noreply.github.com>
2026-04-10 16:59:10 +0200

    Refactor logout method to use Session::destroy()

--- Task 11/Core/Authenticator.php
+++ Task 11/Core/Authenticator.php
@@ -28,10 +28,6 @@ class Authenticator
     }
 
     public function logout() {
-        $_SESSION = [];
-        session_destroy();
-
-        $params = session_get_cookie_params();
-        setcookie('PHPSESSID', '', time() - 3600, $params['domain'], $params['secure'], $params['httponly']);
+        Session::destroy();
     }
 }
